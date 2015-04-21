44d17c83a57f851eff75fef3c8b0590b2c9bea7f
Daniel Goncalves <danieljfgoncalves@gmail.com>
2015-04-21 11:38:27 -0400

    How to make a table view by code only.. IMPORTANTE!!

--- TableViewTheHardWay/AppDelegate.m
+++ TableViewTheHardWay/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "RecipeView.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +18,15 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    // Initiate an instance of RecipeTableViewController
+    RecipeView *recipeTable = [[RecipeView alloc]initWithStyle:UITableViewStylePlain];
+    
+    // Set Window
+    self.window = [[UIWindow alloc]initWithFrame:[[UIScreen mainScreen]bounds]];
+    self.window.rootViewController = recipeTable;
+    [self.window makeKeyAndVisible];
+    
     return YES;
 }
 
---
+++ TableViewTheHardWay/RecipeView.h
@@ -0,0 +1,13 @@
+//
+//  RecipeView.h
+//  TableViewTheHardWay
+//
+//  Created by Daniel Goncalves on 2015-04-21.
+//  Copyright (c) 2015 Daniel J Goncalves. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RecipeView : UITableViewController
+
+@end
